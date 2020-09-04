abad25a109041ba69a1bf34630aac2ff677b9a1b
said ravest <said.ravest@ext.sonda.com>
2020-09-03 21:04:13 -0400

    se dejo el token en universidades

--- routes/universidades.ts
+++ routes/universidades.ts
@@ -1,9 +1,7 @@
 import { Universidades } from './../models/universidades';
 import { Router,Request,Response } from "express";
 import { verificarToken } from "../middlewares/authentication";
-import { Usuario } from "../models/usuario";
 const universidadRouter  = Router();
-const token = "5f45a5022348761538defafe"
 universidadRouter.get('/list',(req:Request, res:Response)=>{
    
     Universidades.find().then(universidadDB => {
@@ -13,16 +11,12 @@ universidadRouter.get('/list',(req:Request, res:Response)=>{
     })
 })
 
-universidadRouter.post('/miuniversidad',verificarToken,(req:any, res:Response) => {
-
-    
-
-    Universidades.findById(/*req.usuario.universidad*/token).then(universidadesDB => {
+universidadRouter.get('/miuniversidad',verificarToken,(req:any, res:Response) => {
+    Universidades.findById(req.usuario.universidad).then(universidadesDB => {
         res.json({
             data:universidadesDB
         })
     })
-
 })
 
 universidadRouter.post('/create',(req:Request, res:Response) => {
